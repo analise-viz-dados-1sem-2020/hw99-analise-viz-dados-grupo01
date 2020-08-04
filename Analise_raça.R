ba2eb46c6f172960fae33c3b96d0ce0e3628c79b
Kamilla Jotadiemel <kjj1474@gmail.com>
2020-08-04 19:45:16 -0300

    Parte 3 - Análise por raça

--- Analise_raça.R
+++ Analise_raça.R
@@ -13,7 +13,7 @@ df_casos_raca <- df %>%
 df_casos_raca %>% 
   mutate(RACA = fct_reorder(RACA, CASOS, .desc = TRUE)) %>% 
   ggplot(aes(x = RACA, y = CASOS))+
-  geom_bar(stat = 'identity', fill = "#836FFF")+
+  geom_bar(stat = 'identity', fill = "#66CDAA")+
   labs(title = "Número de casos confirmados por Raça", x = "Raça", y = "Número de casos confirmados")  
 
 ggsave("img/casos_raça.png")
@@ -27,7 +27,7 @@ casos_raca_inf <- df_casos_raca %>%
 casos_raca_inf %>% 
   mutate(RACA = fct_reorder(RACA, CASOS, .desc = TRUE)) %>% 
   ggplot(aes(x = RACA, y = CASOS))+
-  geom_bar(stat = 'identity', fill = "#836FFF")+
+  geom_bar(stat = 'identity', fill = "#66CDAA")+
   labs(title = "Número de casos confirmados por Raça", x = "Raça", y = "Número de casos confirmados")
 
 ggsave("img/casos_raça_informados.png")
@@ -43,10 +43,10 @@ df_obitos_raca <- df %>%
 df_obitos_raca %>%
   mutate(RACA = fct_reorder(RACA, OBITOS, .desc = TRUE)) %>% 
   ggplot(aes(x = RACA, y = OBITOS))+
-  geom_bar(stat = 'identity')+
+  geom_bar(stat = 'identity', fill = "#D2691E")+
   labs(title = "Número de óbitos por Raça", x = "Raça", y = "Número de óbitos")
 
-ggsave("img/obitos_raça.png") 
+ggsave("img/obitos_raça.png")
 
 ## GRÁFICO DE OBITOS POR RAÇAS - RETIRANDO AS NÃO INFORMADAS
 
@@ -55,10 +55,11 @@ obitos_raca_inf <- df_obitos_raca [-4,]
 obitos_raca_inf %>% 
   mutate(RACA = fct_reorder(RACA, OBITOS, .desc = TRUE)) %>% 
   ggplot(aes(x = RACA, y = OBITOS))+
-  geom_bar(stat = 'identity', fill = "#836FFF")+
+  geom_bar(stat = 'identity', fill = "#D2691E")+
   labs(title = "Número de óbitos por Raça", x = "Raça", y = "Número de óbitos")
 
 ggsave("img/obitos_raça_informados.png")
+
 
 #DATA FRAME E GRÁFICO DE LETALIDADE POR RAÇA
 
@@ -72,7 +73,7 @@ df_letal_raca <- letal_raca %>%
 df_letal_raca %>% 
   mutate(RACA = fct_reorder(RACA, TAXA_LETALIDADE, .desc = TRUE)) %>% 
   ggplot(aes(x = RACA, y = TAXA_LETALIDADE))+
-  geom_bar(stat = 'identity')+
+  geom_bar(stat = 'identity', fill = "#B22222")+
   labs(title = "Taxa de Letalidade por Raça", x = "Raça", y = "Taxa de letalidade (%)")
 
 ggsave("img/letalidade_raca.png")
